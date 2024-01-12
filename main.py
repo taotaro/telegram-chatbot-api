beed5323de11888e7ceaef86e80815fc485059f0
kavyagb22 <kgbalaji@connect.ust.hk>
2024-01-12 11:56:11 +0800

    new changes

--- main.py
+++ main.py
@@ -67,3 +67,5 @@ async def plugin():
 async def serve_openapi_yaml():
     return FileResponse(os.path.join('.', 'openapi.yaml'), media_type='text/yaml')
 
+if __name__ == '__main__':
+    uvicorn.run(app, host='0.0.0.0', port=5000)
